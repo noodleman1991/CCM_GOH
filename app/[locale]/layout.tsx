f63d04e9923c760b21efcc4542406b058fafdef6
noodleman1991 <149662590+noodleman1991@users.noreply.github.com>
2025-11-14 10:34:24 -0500

    plausible 2

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "../globals.css";
 import { cn } from "@/lib/utils";
 
@@ -86,6 +87,18 @@ export default async function LocaleLayout({
                 lato.variable
             )}
         >
+            <Script
+                src="https://plausible.io/js/pa-3hEF5jJ5x-S__sKhqgipY.js"
+                strategy="afterInteractive"
+                async
+            />
+            <Script id="plausible-init" strategy="afterInteractive">
+                {`
+                    window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)};
+                    plausible.init=plausible.init||function(i){plausible.o=i||{}};
+                    plausible.init();
+                `}
+            </Script>
             <ClerkProvider localization={clerkLocalization}>
                 <NextIntlClientProvider messages={messages}>
                     <ThemeProvider
